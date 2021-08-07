f9843d3dc0ef668029fdaa19277d1bfc46638722
Akash Das <mfs.akash@gmail.com>
2021-08-07 15:36:39 +0530

    Added a private constructor to demo class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
-public class HelloWorld
-{
+public class HelloWorld {
+
+    private HelloWorld() {
+        throw new IllegalStateException("Demo class");
+    }
+
     public static void main() {
         System.out.println("HelloWorld");
     }
